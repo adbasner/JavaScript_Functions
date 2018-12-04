24146d85804b59f7580130a3a71df3885fc81ffd
Andrew Basner <andrewbasner@gmail.com>
2018-12-04 16:28:41 -0600

    factorial.js update, removed recursion

--- FreeCodeCamp/factorial.js
+++ FreeCodeCamp/factorial.js
@@ -2,11 +2,16 @@ function factorialize(num) {
   if (num === 0) {
     return 1;
   } else {
-    return num * factorialize(num - 1);
+    let product = num;
+    for (let i = num - 1; i > 1; i--) {
+      product *= i;
+    }
+    return product;
   }
 }
 
 console.log("5! = " + factorialize(5)); //120
-// console.log("10! = " + factorialize(3628800)); // 120
-// console.log("20! = " + factorialize(20)); // 2432902008176640000120
+console.log("10! = " + factorialize(10)); // 3628800
+console.log("20! = " + factorialize(20)); // 2432902008176640000
 console.log("0! = " + factorialize(0)); //1
+
