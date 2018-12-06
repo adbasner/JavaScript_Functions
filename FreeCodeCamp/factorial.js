b3275ece306e839a8f6c8b9fc53b77af80c69f0e
Andrew Basner <andrewbasner@gmail.com>
2018-12-06 11:02:14 -0600

    clean up factorial

--- FreeCodeCamp/factorial.js
+++ FreeCodeCamp/factorial.js
@@ -1,4 +1,10 @@
 function factorialize(num) {
+  // if (num === 0) {
+  //   return 1;
+  // } else {
+  //   return num * factorialize(num - 1);
+  // }
+
   if (num === 0) {
     return 1;
   } else {
@@ -10,8 +16,9 @@ function factorialize(num) {
   }
 }
 
+
 console.log("5! = " + factorialize(5)); //120
 console.log("10! = " + factorialize(10)); // 3628800
 console.log("20! = " + factorialize(20)); // 2432902008176640000
 console.log("0! = " + factorialize(0)); //1
-
+console.log("0! = " + factorialize(232323)); //1
